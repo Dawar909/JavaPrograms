c59c88183c2e67107eb3d5eadeb8942f86afac97
Dawar Ashai <ashai.dawar0@gmail.com>
2024-10-07 18:00:23 +0530

    Removed Controversial Topics

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,5 @@ class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
-        System.out.println("Nigga");
     }
 }
